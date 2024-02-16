0e3dc56d66fcdde53c3c95bce74bc01f915db1da
Abdullah Emin BAYRAKLILAR <abdullahbayraklilar@gmail.com>
2024-02-16 20:05:41 +0300

    Updated config.py to add BTC AVAX and STX as currency

--- superset/config.py
+++ superset/config.py
@@ -375,12 +375,12 @@ LANGUAGES = {}
 
 # Override the default d3 locale format
 # Default values are equivalent to
-# D3_FORMAT = {
-#     "decimal": ".",           # - decimal place string (e.g., ".").
-#     "thousands": ",",         # - group separator string (e.g., ",").
-#     "grouping": [3],          # - array of group sizes (e.g., [3]), cycled as needed.
-#     "currency": ["$", ""]     # - currency prefix/suffix strings (e.g., ["$", ""])
-# }
+D3_FORMAT = {
+    "decimal": ".",           # - decimal place string (e.g., ".").
+    "thousands": ",",         # - group separator string (e.g., ",").
+    "grouping": [3],          # - array of group sizes (e.g., [3]), cycled as needed.
+    "currency": ["$", "🪙" "₿", "🪙"]     # - currency prefix/suffix strings (e.g., ["$", ""])
+}
 # https://github.com/d3/d3-format/blob/main/README.md#formatLocale
 class D3Format(TypedDict, total=False):
     decimal: str
@@ -389,9 +389,9 @@ class D3Format(TypedDict, total=False):
     currency: list[str]
 
 
-D3_FORMAT: D3Format = {}
+# D3_FORMAT: D3Format = {}
 
-CURRENCIES = ["USD", "EUR", "GBP", "INR", "MXN", "JPY", "CNY", "AVAX", "BTC", "STX"]
+CURRENCIES = ["USD", "AVAX", "BTC", "STX"]
 
 # ---------------------------------------------------
 # Feature flags
